acafb56f4a02021f20a5c662eea182cd1b66ede1
khaled <khaled@example.com>
2023-06-12 11:44:23 +0300

    modify backend state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4699cc30ff8059e9a32df2c1fc4881c68e13b5e916c0e6b624cce54c88980412
-size 2203
+oid sha256:a4739d83fc21e0ea66db5ab8d8b0b9d5a161fe6d8821d9f3a3d388e44e81e371
+size 2201
