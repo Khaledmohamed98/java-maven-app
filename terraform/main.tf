e5c8e04790a2f5abb77c494874353ccfe66c4a81
khaled <khaled@example.com>
2023-06-12 09:05:43 +0300

    modify jenkins ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aee66606b774b80a44f807db7ed7b576cf49cc8c46e9202b47c629624e5f0fac
-size 2372
+oid sha256:527f933662042e21b72faed4e8b9a0a0ee9711e6d23333b0ad8c8b42c61edd7a
+size 2250
