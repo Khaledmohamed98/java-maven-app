22951c865b92780356554d9c414b1d30d3658b31
khaled <khaled@example.com>
2023-06-12 09:01:32 +0300

    modify jenkins ip

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90f033996ba529bf74dcd5f01f2a586fb775c47f44e08eb5b74c872943099aa7
+oid sha256:e784e8af09c6ba78eb7bfec879dd548ba3d5682fdaf8314efbb5c73b09742938
 size 413
