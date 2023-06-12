af9d7f5e23a309c4171fa7776fea44a6f3fd1ae3
khaled <khaled@example.com>
2023-06-12 11:55:26 +0300

    modify avail zone

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37c00c6f6017f6a6fdce8a63e4d352e3ed805ca9d4d347b6a9d9878e9d2a21e3
+oid sha256:da27b25ac9f864b5202c7cf5d1ea333f46aa4a4b22b57ccc902358c46d83f5bd
 size 420
